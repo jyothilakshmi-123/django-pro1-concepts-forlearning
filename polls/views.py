3b1940dcadee0caeb3f072d3402c287bd3ab36e4
Jyothilakshmi C <jyothilakshmi1491@gmail.com>
2020-07-06 11:08:21 +0530

    added previously error occered to master

--- polls/views.py
+++ polls/views.py
@@ -4,5 +4,12 @@ from django.shortcuts import render
 def index(request):
     return render(request,'home.html',{'name':'jyothi'})
 
+def add(request):
+    val1 = int(request.GET['num1'])
+    val2 = int(request.GET['num2'])
+    res  = val1 + val2
+
+    return render(request,'result.html',{'result':res})
+
 
 # Create your views here.
